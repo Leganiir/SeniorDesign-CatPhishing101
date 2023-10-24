00975e8db7491bdc677264b83e74c608bfa2c961
hamdanzd <143369477+hamdanzd@users.noreply.github.com>
2023-10-24 17:48:07 -0400

    Update blacklist.py

--- blacklist.py
+++ blacklist.py
@@ -20,3 +20,36 @@ test = blacklist("fotapek847@wisnick.com")
 print(test)
 test = blacklist("isaacleganik@gmail.com")
 print(test)
+
+
+
+
+
+
+
+#################
+Edited by zaid
+
+def blacklist(email):
+    blacklist = ["wermink.com", "beaconmessenger.com", "lyft.live", "socam.me", "tutuapp.bid", "yogrow.co", "afia.pro", "clout.wiki",
+                 "hexi.pics", "wisnick.com", "weirby.com"]
+    
+    domain = email.split("@")[1]
+    
+    # Searches through the blacklist database, if the domain is a match, it returns a boolean True, otherwise it returns a False.
+    for domains in blacklist:
+        if domain == domains:
+            return True
+    
+    return False
+
+# Input email address from the user
+user_email = input("Enter your email address: ")
+
+# Call the blacklist function and print the result
+is_blacklisted = blacklist(user_email)
+
+if is_blacklisted:
+    print("The email domain is blacklisted.")
+else:
+    print("The email domain is not blacklisted.")
